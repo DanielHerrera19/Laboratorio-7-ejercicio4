56804d9b2449a59ba914053d7580de78a0b90cbf
Daniel <Daniel@DESKTOP-KKC0TQU>
2023-10-04 21:33:32 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_7_ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                Console.Write("Ingresa un número: ");
+                int num = int.Parse(Console.ReadLine());
+                int j = 0;
+                while (j < num)
+                {
+                    int k = 0;
+                    while (k < num * 2)
+                    {
+                        Console.Write("*");
+                        k++;
+                    }
+                    Console.WriteLine();
+                    j++;
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
